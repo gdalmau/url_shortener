f6da1f7106f2e3822d3448eadf9561ef313ab43c
Gerard Dalmau <gddalmau@gmail.com>
2019-10-13 14:55:47 +0200

    NEW add library setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt', 'r') as f:
+    INSTALL_REQUIRES = f.readlines()
+
+setup(
+    name='url_shortener',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/gdalau/url_shortener',
+    license='MIT',
+    author='Gerard Dalmau',
+    description='Webservice to shorten URLs',
+    install_requires=INSTALL_REQUIRES
+)
